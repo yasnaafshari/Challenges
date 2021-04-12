1497f742e27d262fe53a72886c103290e04eb373
yasna <yasnaafshari2123@gmail.com>
2021-04-12 22:49:06 +0430

    second challenge

--- app/src/main/java/com/example/challenges/MainActivity.kt
+++ app/src/main/java/com/example/challenges/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.challenges
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val service : UsersAPI = UserNetwork.retrofit
+        service.getUsers().enqueue(object : Callback,
+            retrofit2.Callback<List<UserModel>> {
+            override fun onResponse(
+                call: Call<List<UserModel>>,
+                response: Response<List<UserModel>>
+            ) {
+                var list : List<UserModel>? = response.body()
+            }
+
+            override fun onFailure(call: Call<List<UserModel>>, t: Throwable) {
+            }
+
+        })
+
+
     }
+
+
 }
