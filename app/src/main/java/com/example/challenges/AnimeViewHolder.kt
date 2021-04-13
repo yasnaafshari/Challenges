1431726429f6aeb2f38a102b3d81966a7a19ed3f
yasna <yasnaafshari2123@gmail.com>
2021-04-13 22:12:15 +0430

    third challenge

---
+++ app/src/main/java/com/example/challenges/AnimeViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.challenges
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AnimeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val userName = itemView.findViewById<TextView>(R.id.userName)!!
+    val userType = itemView.findViewById<TextView>(R.id.userType)!!
+}
